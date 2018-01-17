b98317f0df0969479024f701f5c828ecdd0ef196
waderice <waderice52224@gmail.com>
2018-01-16 20:02:22 -0700

    second night push

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var CheckingAccount_1 = require("./CheckingAccount");
 var SavingsAccount_1 = require("./SavingsAccount");
+var retirment_1 = require("./retirment");
+var checking = new CheckingAccount_1.CheckingAccount();
+var retirement = new retirment_1.Retirement();
 var savings = new SavingsAccount_1.SavingsAccount();
 console.log(savings.withdrawMoney(10, "good", 1));
 console.log(savings.success);
 console.log(savings.balance);
+console.log(savings.description);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import { CheckingAccount} from "./CheckingAccount";
 import { SavingsAccount} from "./SavingsAccount";
 import {Transaction} from "./Transaction";
+import {Retirement} from "./retirment";
+
+
+let checking = new CheckingAccount();
+
+let retirement = new Retirement();
 
 let savings = new SavingsAccount();
+
 
 console.log(savings.withdrawMoney(10, "good", 1));
 console.log(savings.success);
 console.log(savings.balance);
+console.log(savings.description);
